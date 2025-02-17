968bee33775bf500fe6e57f67b2773b21fc97a0c
Viswaganth <viswaganth109@gmail.com>
2025-02-17 15:45:25 +0530

    day 3 and day 4

---
+++ web/script.js
@@ -0,0 +1,21 @@
+let a="viswa"
+a=10
+a=false
+console.log(a)
+const b="Eren"
+console.log(b)
+let array1=[a,b,true,10,"hello"]
+console.log(array1)
+console.log(array1[1])
+let jsobj={
+    name:"a",
+    age:"12",
+    isalive:true
+}
+let jsonobj={
+       "name": "a",
+       "age":"12",
+       "isalive":true
+}
+console.log(jsobj)
+console.log(jsonobj)
